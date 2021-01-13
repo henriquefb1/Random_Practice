f2b433c1d16527a6ef1cb421ddc7fbd26ba9807e
Henrique Fernandes Barreiros <70530577+henriquefb1@users.noreply.github.com>
2021-01-13 10:43:57 -0300

    Scraping practice
    First attempt on Scraping with Beautifulsoup, scraping a page from Stackoverflow, showing all questions tagged as Python, with their titles, date and vote count.

---
+++ scraping.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://pt.stackoverflow.com/questions/tagged/python'
+response = requests.get(url)
+html = BeautifulSoup(response.text, 'html.parser')
+
+for pergunta in html.select('.question-summary'):
+    titulo = pergunta.select_one('.question-hyperlink')
+    time = pergunta.select_one('.relativetime')
+    voto = pergunta.select_one('.votes')
+    print(time.text, titulo.text, voto.text, sep='\t')
